77e6f19a1dcb9f3b0efac3417cf64e5b98c7eb98
Max Tao <maxtao989@gmail.com>
2022-07-29 21:06:28 -0700

    class files

---
+++ src/main/java/jda/standardcommand/DatabaseHandling.java
@@ -0,0 +1,48 @@
+package jda.standardcommand;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import jda.Config;
+import jda.command.CommandContext;
+import jda.command.ICommand;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.Set;
+
+public class DatabaseHandling implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx) throws IOException, GeneralSecurityException {
+        String accesskey = Config.get("MONGODBCLIENT");
+        MongoClient mongo = new MongoClient("localhost");
+        List<String> databases = mongo.getDatabaseNames();
+        for (String dbName: databases){
+            System.out.println("Database: " + dbName);
+            DB db = mongo.getDB(dbName);
+            Set<String> collections = db.getCollectionNames();
+            for (String colName : collections){
+                System.out.println("\t + Collection: " + colName);
+            }
+        }
+        mongo.close();
+
+
+
+
+
+
+    }
+
+    @Override
+    public String getName() {
+        return "mongo";
+    }
+
+    @Override
+    public String getHelp() {
+        return "access data";
+    }
+}
